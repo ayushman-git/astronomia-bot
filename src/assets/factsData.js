59e5ea041ad7b5fe43e3c57aec3657994132e47d
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-10-16 20:32:20 +0530

    added facts

--- src/assets/factsData.js
+++ src/assets/factsData.js
@@ -14,4 +14,284 @@ module.exports = [
     description:
       "Io's eruptions come from the immense gravity the moon is exposed to, being nestled in Jupiter's gravitational well. The moon's insides tense up and relax as it orbits closer to, and farther from, the planet, generating enough energy for volcanic activity. Scientists are still trying to figure out how heat spreads through Io's interior, though, making it difficult to predict where the volcanoes exist using scientific models alone.",
   },
+  {
+    title: "Enceladus, one of Saturn’s smaller moons, reflects 90% of the Sun’s light",
+    description:
+      "Because Enceladus’ icy surface reflects sunlight rather than absorbing it, temperatures reach as low as -394° Fahrenheit (-201° Celsius).",
+  },
+  {
+    title: "The highest mountain discovered is the Olympus Mons, which is located on Mars",
+    description:
+      "Its peak is 16 miles (25 km) high, making it nearly 3 times higher than Mount Everest. And not only is it tall, but it’s also 374,015 ft² (114,000 m²) wide – that’s an area the size of Arizona!",
+  },
+  {
+    title: "The Whirlpool Galaxy (M51) was the first celestial object identified as being spiral",
+    description:
+      "The grand, spiraling arms of the Whirlpool Galaxy are made up of long lanes of stars and gas, sprinkled with lots of space dust. These arms act as star formation factories, compressing hydrogen gas and creating clusters of new stars.",
+  },
+  {
+    title: "A light-year is the distance covered by light in a single year",
+    description:
+      "Light moves at the velocity of about 186,411 miles (300,000 km) a second. So one light-year equates to roughly 5,903,026,326,255 miles (9,460,730,777,119 km)!",
+  },
+  {
+    title: "The Milky Way galaxy is 105,700 light-years wide",
+    description:
+      "It would take a modern spacecraft 450,000,000 years to travel to the center of our galaxy!",
+  },
+  {
+    title: "The Sun weighs about 330,000 times more than Earth",
+    description:
+      "It is about 109 times the diameter of Earth and is so large the Earth could fit inside the sun about 1,300,000 times over! In fact, the sun is so gigantic that it contains 99.85% of all mass in our solar system.",
+  },
+  {
+    title: "Footprints left on the Moon won’t disappear as there is no wind",
+    description:
+      "But wait a minute… if there’s no wind to blow them away then why is the flag blowing? Well, it actually wasn’t blowing. That rippling you see is because of a stubborn telescopic horizontal rod that the astronauts were struggling to remove from the flag’s upper hem.",
+  },
+  {
+    title: "There are 79 known moons orbiting Jupiter",
+    description:
+      "Jupiter is the planet in our solar system with the most moons, and it also has the largest moon in our solar system. That moon is called Ganymede and is 33,279 miles (5,262 km) in diameter – that’s bigger than Mercury and it is visible with just a pair of binoculars!",
+  },
+  {
+    title: "The Martian day is 24 hours 39 minutes and 35 seconds long",
+    description:
+      "So you’d therefore assume there are less days in a year on Mars than Earth, right? Wrong! Because Mars orbits the sun slower than the Earth, there are actually 687 Martian days in a Martian year!",
+  },
+  {
+    title: "The Sun makes a full rotation once every 25 – 35 days",
+    description:
+      "So for us on Earth, one full rotation equals one full day. However, our gargantuan sun takes 25 – 35 Earth days to make one full rotation!",
+  },
+  {
+    title: "Earth is the only planet not named after a God",
+    description:
+      "Nobody knows how the Earth got its name; all we know is that it is derived from an amalgamation of both the Old English and Old Germanic words for “ground”.",
+  },
+  {
+    title: "Pluto is smaller than the United States",
+    description:
+      "If you were to walk around the equator of Pluto it would be the same distance as walking from London to Denver (well, give or take 56 miles).",
+  },
+  {
+    title: "According to mathematics, white holes are possible, although as of yet we have found none",
+    description:
+      "A white hole is a hypothetical region of space-time which can’t be entered from the outside, although matter and light can escape from within. Basically, it’s the reverse of a black hole.",
+  },
+  {
+    title: "Because of its unique tilt, a season on Uranus is equivalent to 21 Earth years",
+    description:
+      "Also the 97.77 degrees tilt on Uranus’ axis means that a day there only last 17 hours, 14 minutes and 24 seconds.",
+  },
+  {
+    title: "There are more stars in space than there are grains of sand in the world",
+    description:
+      "There are 10 times more stars in the night sky than grains of sand on the Earth, with 70 sextillion stars being visible from Earth through a telescope. To put that in numbers, 70 sextillion is this: 70,000,000,000,000,000,000,000.",
+  },
+  {
+    title: "Any free-moving liquid in outer space will form itself into a sphere",
+    description:
+      "This is because of something called surface tension, which is an imbalance of intermolecular attractive forces. This can also occur in low Earth orbit.",
+  },
+  {
+    title: "We know more about Mars and our Moon than we do about our oceans",
+    description:
+      "We have fully mapped 100% of the surface of Mars and Earth’s Moon, whereas we have only been able to map roughly 5% of the ocean floor.",
+  },
+  {
+    title: "Light travels from the Sun to the Earth in less than 10 minutes",
+    description:
+      "The Photons emitted from the Sun’s surface travel across the vacuum of space at the speed of light to reach our eyes in only 8 minutes and 20 seconds.",
+  },
+  {
+    title: "The Earth’s rotation is slowing slightly as time goes on",
+    description:
+      "This means that days were shorter in the past. This is because of the tidal effects the Moon has on the Earth’s rotation.",
+  },
+  {
+    title: "Outer Space is only 62 miles away",
+    description:
+      "Although there is no official solid boundary for where space begins, the Kármán line sits at 62 miles above sea-level and is conventionally used as the start of outer space in space treaties or for aerospace records keeping.",
+  },
+  {
+    title: "The International Space Station circles Earth every 92 minutes",
+    description:
+      "The speed of the ISS as it orbits Earth is roughly 17,150 miles per hour(27,600 km/h) – that equates to 5 miles a second(7.66 km/s)!",
+  },
+  {
+    title: "We always see the same side of the Moon, no matter where we stand on Earth",
+    description:
+      "This is because the Moon rotates on its axis at the same rate that it rotates the Earth. It’s known as synchronous rotation or tidal locking.",
+  },
+  {
+    title: "There are approximately 100 thousand million stars in the Milky Way",
+    description:
+      "Out of all the known galaxies, the Milky Way is the one that contains the most stars.",
+  },
+  {
+    title: "Using the naked eye, you can see 3 – 7 different galaxies from Earth",
+    description:
+      "You can see the Andromeda Galaxy (M-31), both Magellanic Clouds, our own Milky Way galaxy, the Triangulum Galaxy (M-33), the Omega Centauri and the Sagittarius Dwarf Spheroidal Galaxy.",
+  },
+  {
+    title: "In 2016, scientists detected a radio signal from a source 5 billion light-years away",
+    description:
+      "This means that when the signal started its journey, Earth didn’t even exist. The detected signals were located using the Very Large Array (VLA) of the National Radio Astronomy Observatory in New Mexico.",
+  },
+  {
+    title: "The closest galaxy to us is the Andromeda Galaxy – it’s estimated at 2.5 million light-years away",
+    description:
+      "Before the discovery of the Andromeda Galaxy, the Large Magellanic Cloud was believed to be the closest galaxy to us.",
+  },
+  {
+    title: "The first ever black hole photographed is 3 million times the size of Earth",
+    description:
+      "The photo was released in April 2019 and shows a halo of dust and gas 310 million trillion miles from Earth. It was captured by the Event Horizon Telescope, a network of eight linked telescopes, and was also captured due to the algorithm of programr Katie Bouman.",
+  },
+  {
+    title: "The distance between the Sun & Earth is defined as an Astronomical Unit",
+    description:
+      "An Astronomical Unit (AU) equates to roughly 93 million miles, or 150 million kilometers.",
+  },
+  {
+    title: "Space is completely silent",
+    description:
+      "This is because there is no air in space, and air is needed to carry the sound vibrations. So if you shouted to someone next to you in space they wouldn’t be able to hear you.",
+  },
+  {
+    title: "Astronauts can grow approximately two inches (5 cm) in height when in space",
+    description:
+      "This is due to the lack of gravity in space causes the discs between the vertebrae to expand a little. However, this extra height is lost when re-entering the Earth’s atmosphere and being subjected to the Earth’s gravity again.",
+  },
+  {
+    title: "The Kuiper Belt is a region of the Solar System beyond the orbit of Neptune",
+    description:
+      "The Kuiper Belt is a ring of icy bodies and is where Pluto is located.",
+  },
+  {
+    title: "The Hubble Space Telescope is one of the most productive scientific instruments ever built",
+    description:
+      "Astronomers using Hubble data have published more than 15,000 scientific papers. Those papers have been cited in other papers 738,000 times.",
+  },
+  {
+    title: "Exoplanets are planets that orbit around other stars",
+    description:
+      "All of the planets in our solar system orbit around the Sun. But what about other solar systems? In 2009, NASA launched a spacecraft called Kepler to look for exoplanets, and has discovered thousands since its launch.",
+  },
+  {
+    title: "Our moon is moving away from Earth at a rate of 1.6 inch (4 cm) per year!",
+    description:
+      "Scientists do believe that eventually the Moon will move out of the field of Earth’s gravity; however this won’t happen for billions of years to come.",
+  },
+  {
+    title: "Pluto is named after the Roman god of the underworld, not the Disney Dog",
+    description:
+      "The name for the planet was suggested by Venetia Burney, an eleven-year old British schoolgirl, to the planet’s discoverer Clyde Tombaugh.",
+  },
+  {
+    title: "The ISS is visible to more than 90% of the Earth’s population",
+    description:
+      "When you see the International Space Station (ISS) in the night sky it appears as a fast-moving star crossing from horizon to horizon.",
+  },
+  {
+    title: "A sunset on Mars is blue",
+    description:
+      "Mars has less than 1% of the Earth’s atmosphere. So the sunsets on Mars appear as blue due to the way the blue light from the Sun is captured within the atmosphere of Mars.",
+  },
+  {
+    title: "The first living mammal to go into space was a dog named “Laika” from Russia",
+    description:
+      "Laika was a stray mongrel from the streets of Moscow and was launched into space on the Soviet spacecraft Sputnik 2 on 3rd November 1957. Sadly, Laika died 5-7 hours into the flight due to overheating and stress. Poor doggo.",
+  },
+  {
+    title: "The word “astronaut” means “star sailor” in its origins",
+    description:
+      "It is derived from the Greek words “astron”, meaning “star”, and “nautes”, which means “sailor”. So, the word astronaut literally means “star sailor”.",
+  },
+  {
+    title: "Mercury has no atmosphere, which means there is no wind or weather",
+    description:
+      "Instead of an atmosphere, Mercury possesses a thin exosphere made up of atoms blasted off the surface by the solar wind and striking meteoroids.",
+  },
+  {
+    title: "Red Dwarf stars that are low in mass can burn continually for up to 10 trillion years!",
+    description:
+      "A Red Dwarf is a small and cool star in a later stage of its life and has a surface temperature of less than 7,200k degrees Fahrenheit.",
+  },
+  {
+    title: "Scientists once believed that the same side of Mercury always faced the Sun",
+    description:
+      "However, in 1965 astronomers discovered that the planet rotates three times during every two orbits it makes.",
+  },
+  {
+    title: "Jupiter’s Red Spot is shrinking",
+    description:
+      "Jupiter’s red spot is a huge swirling hurricane-like storm that used to be three times the size of Earth! However, the storm is shrinking over time, but even as it shrinks it gets taller. Scientists are still stumped as to what’s causing this although they believe it may be to do with jet streams on Jupiter changing location or direction.",
+  },
+  {
+    title: "A large percentage of asteroids are pulled in by Jupiter’s gravity",
+    description:
+      "For this reason, Jupiter is known as the dumping grounds for our solar system. Many of the asteroids that are potentially harmful to Earth, the long period comets, tend to be sucked into Jupiter’s gravity field. Thanks Jupiter!",
+  },
+  {
+    title: "A day on Mercury is equivalent to 58 Earth days",
+    description:
+      "This is because Mercury rotates on its axis very slowly compared to Earth.",
+  },
+  {
+    title: "As space has no gravity, pens won’t work",
+    description:
+      "Normal pens work by gravity pulling the ink towards the pen’s nib (the writing part) – as you hold the pen in your hand writing part facing downwards. As there’s no gravity in space, the ink doesn’t get pulled to the nib. However, special pens have been made that work in zero gravity.",
+  },
+  {
+    title: "On average it takes the light only 1.3 seconds to travel from the Moon to Earth",
+    description:
+      "The distance between the Earth and the Moon is only 238,855 miles (384,400 kilometers).",
+  },
+  {
+    title: "There are 88 recognized star constellations in our night sky",
+    description:
+      "These 88 constellations cover the Earth’s night sky and can be observed from the southern and northern hemispheres.",
+  },
+  {
+    title: "As early as 240BC the Chinese began to document the appearance of Halley’s Comet",
+    description:
+      "After 164BC there was a continuous recording of the comet each time it was visible.",
+  },
+  {
+    title: "In 2006, the International Astronomical Union reclassified Pluto as a dwarf planet",
+    description:
+      "This is because Pluto does not gravitationally dominate the neighborhood around its orbit.",
+  },
+  {
+    title: "There are 5 Dwarf Planets recognized in our Solar System",
+    description:
+      "The Dwarf Planets are Ceres, Makemake, Haumea, Eris and Pluto. The dwarf planet Ceres is also the largest asteroid in our solar system, and resides in the asteroid belt, making it the only dwarf planet to not inhabit the outer solar system.",
+  },
+  {
+    title: "Mars is the most likely planet in our solar system to be hospitable to life",
+    description:
+      "In 1986, NASA found what they believe may be fossils of microscopic living organisms in a rock recovered from the surface of Mars.",
+  },
+  {
+    title: "Halley’s Comet will pass over Earth again on 26th July 2061",
+    description:
+      "The famous comet was last seen on 9th February, 1986 and only orbits the Earth once every 75 – 76 years.",
+  },
+  {
+    title: "There is a planet half the radius of the Earth with a surface made up of diamonds",
+    description:
+      "55 Cancri e has a mass eight times that of Earth’s despite having half the radius, and may very well have a surface made up of graphite and diamonds. It is only 40 light-years away and visible to the naked eye under the constellation of Cancer.",
+  },
+  {
+    title: "There is a supermassive black hole at the heart of every galaxy",
+    description:
+      "Active galaxies often pump out 100 times more light than a normal galaxy. With the discovery in 1963 of quasars, it was clear that the light comes not from stars but from a central region smaller than the Solar System. The only conceivable energy source is matter heated to incandescence as its swirls down onto a giant black hole up to 50 billion times the mass of the Sun. In the 1990s, NASA’s Hubble Space Telescope found that, although active galaxies account for only about 1% of galaxies, supermassive black holes are no anomaly. Almost every galaxy, including our Milky Way, contains one, but starved of a food supply, most have switched off.",
+  },
+  // {
+  //   title: "",
+  //   description:
+  //     "",
+  // },
 ];
