afa2c077f17c8ab8bf675fc5e7757bfb353faa82
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-10-21 18:09:07 +0530

    added facts

--- src/assets/factsData.js
+++ src/assets/factsData.js
@@ -289,6 +289,262 @@ module.exports = [
     description:
       "Active galaxies often pump out 100 times more light than a normal galaxy. With the discovery in 1963 of quasars, it was clear that the light comes not from stars but from a central region smaller than the Solar System. The only conceivable energy source is matter heated to incandescence as its swirls down onto a giant black hole up to 50 billion times the mass of the Sun. In the 1990s, NASA’s Hubble Space Telescope found that, although active galaxies account for only about 1% of galaxies, supermassive black holes are no anomaly. Almost every galaxy, including our Milky Way, contains one, but starved of a food supply, most have switched off.",
   },
+  //2nd part
+  {
+    title: "A unit called parsec is even longer than a light year",
+    description:
+      "Measuring 3.26 light years, it is a function of an astronomical unit and a parallax angle of 1 degree. Its name is derived from “parallax of one arcsecond”.",
+  },
+  {
+    title: "The biggest known star currently is UY Scuti",
+    description:
+      "UY Scuti is a bright red supergiant, located 9500 light years away in the Scutum constellation. It is more than 1,708 times the size of the Sun.If UY Scuti was placed in our system instead of the Sun, it would engulf everything up to the orbit of Saturn.",
+  },
+  {
+    title: "Galaxies can collide",
+    description:
+      "In fact, our own milky way is due a collision with Andromeda galaxy. Not to worry though, this is going to happen in over 4 billion years from now, and even when it happens, galactic distances are so vast that the chances of a star or a planet hitting each other are almost non-existent.",
+  },
+  {
+    title: " The biggest galaxy is over 30 times bigger than our own modest Milky Way",
+    description:
+      "The IC 1101 is a supergiant elliptical galaxy and it spans over 5.8 million light years, with more than a 100 trillion stars that call it home.",
+  },
+  {
+    title: "The Earth is actually closest to the Sun during January and farthest during July",
+    description:
+      "The seasons we experience are due to the tilt of the Earth’s axis, which makes certain areas get more sunshine at the different parts of its revolution.",
+  },
+  {
+    title: "Earth’s orbit starts at an altitude of 2000 km",
+    description:
+      "If we were to build a staircase from the sea level to orbit, it would take two weeks to climb.",
+  },
+  {
+    title: "The only natural satellite that orbits the Earth is Moon",
+    description:
+      "It is one of the largest and densest satellites, but it doesn’t have any atmosphere. The Moon is in synchronous orbit(tidally locked) around the Earth, which means that we always see the same side of the Moon.",
+  },
+  {
+    title: "There is no dark side of the Moon",
+    description:
+      "The side that we don’t see is illuminated by the Sun as often as the side that we face.",
+  },
+  {
+    title: "Every year there are at least 2 lunar (Moon) eclipses, but it can happen that there are no solar eclipses",
+    description:
+      "While the solar eclipses are rare, they still occur at least once every 18 months. The reason why we feel it’s much rarer than that is because they can be seen only from a fairly small area on the Earth.",
+  },
+  {
+    title: "Lunar eclipse happens when the Moon passes into the Earth’s umbra (shadow)",
+    description:
+      "This means that Earth is blocking the sunlight that usually falls on the Moon. This event can happen up to four times per year.",
+  },
+  {
+    title: "A solar eclipse occurs when the Moon passes right in front of the Sun, and it casts its shadow on Earth",
+    description:
+      "The area of Earth that is in the Moon’s shadow is the only part on Earth where that particular eclipse can be seen. This is the only time when we can see the Sun’s corona from the Earth.",
+  },
+  {
+    title: "The red planet(Mars) is also known for having some of the most impressive surface features",
+    description:
+      "It is the home of Olympus Mons, the largest volcano in the solar system, and Valles Marineris a vast network of Canyons, second only to the rift valleys on Earth.",
+  },
+  {
+    title: "Mars has two tiny moons",
+    description:
+      "Phobos and Deimos. It is believed that these moons are actually asteroids that have been caught up in the Mars’s asteroid field. The names of these satellites mean Fear and Dread, which is very suitable for the companions of the roman god of war.",
+  },
+  {
+    title: "Each of the outer planets have rings around them",
+    description:
+      "But only Saturn’s are visible to us from Earth. These rings are composed of small chunks of ice and rock, and they are surprisingly thin (less than a kilometer thick).",
+  },
+  {
+    title: "All the dwarf planets, except Ceres, are called the Plutoids",
+    description:
+      "In honor of the ex-planet Pluto. This designation is reserved for all the dwarf planets that orbit the Sun beyond Neptune.",
+  },
+  {
+    title: " Only three people have died in space",
+    description:
+      "Russian astronauts Georgy Dobrovolsky, Vladislav Volkov, and Viktor Patsayev, the crew of Soyuz 11, suffered a malfunction while returning to Earth, at the distance of 168 km. Their bodies have been successfully recovered, and they were given a hero’s funeral. Unfortunately, there are many more astronauts that died in Earth’s atmosphere during the launch or re-entry of their spacecraft. Crews of space shuttles Challenger and Columbia have been tragically lost, and many astronauts and cosmonauts have perished during various training exercises or test launches, over the years.",
+  },
+  {
+    title: "Amazingly, humans can survive in space unprotected, for a short time",
+    description:
+      "A human without a space suit can survive the rigors of vacuum for around 30 seconds to a minute conscious, and several more minutes blacked out, if the rescue came in time.",
+  },
+  {
+    title: "There were three modular space stations built, of which 2 are still functional",
+    description:
+      "The first one, the Russian Mir station was operational for 25 years, before it was abandoned, and subsequently made a controlled burn out in the atmosphere.",
+  },
+  {
+    title: "Both voyagers carry a golden record",
+    description:
+      "This record contains the sounds from Earth, selected by a special committee, and on its cover are etched symbolic directions to our home planet. The Pale Blue Dot is a famous photo with a commentary by Carl Sagan, which is also made by Voyager 1.",
+  },
+  {
+    title: "Tardigrades are the most resilient organisms known to man",
+    description:
+      "These micro-animals, only half a millimeter large can survive unprotected in space for 10 days.",
+  },
+  {
+    title: "All our satellites, probes and rockets leave a lot of garbage behind",
+    description:
+      "In fact, there is a debris field surrounding Earth with more than 500 000 pieces larger than one centimeter. These pieces are routinely tracked by stations on Earth, in order to control potential damage to our satellites and stations.",
+  },
+  {
+    title: "New horizons is the first probe we sent to investigate Pluto and some of the Kuiper belt objects",
+    description:
+      "It also carries a number of Earth mementos on it, among which are the ashes of Clyde Tombaugh, the man who discovered Pluto in 1930.",
+  },
+  {
+    title: "Stars are classified by their color, size and temperature",
+    description:
+      "Our Sun is a GV2 type yellow dwarf, and it is a main sequence star. (These main sequence stars are the most common.)",
+  },
+  {
+    title: "Stars exist in the state that is called plasma",
+    description:
+      "And it is actually the fourth fundamental state of matter.This is actually the most abundant form of ordinary matter (yes, there is unordinary matter) in the universe, even though we don’t witness it on Earth at all, except in the labs. (Well, and if you cut a grape in half and microwave it. – do not do this at home!)",
+  },
+  {
+    title: "A protostar is a star infant of a sort",
+    description:
+      "This is the name used for a star that is still gathering mass from its parent molecular cloud (Nebula). This is the first stage of the star evolution, and it lasts for about a billion years for stars that are similar in size to our own Sun.",
+  },
+  {
+    title: "Stars are born in nebulae",
+    description:
+      "Stars are born in nebulae",
+  },
+  {
+    title: "Although we see stars as specks of light in the sky, sometimes that speck is not just one star",
+    description:
+      "Binary star systems have two stars instead of one that orbit around each other. The craziest part is that the orbiting objects don’t have to be stars. They can be black holes, neutron stars, white dwarfs, or any other celestial body.",
+  },
+  {
+    title: "Besides binary, there are also other multiple star systems",
+    description:
+      "The most common of these are trinary systems, where three stars orbit around each other.",
+  },
+  {
+    title: "The most complex multiple systems we have discovered are septuple star systems",
+    description:
+      "Septuple star system means that there are 7 star orbiting around each other in a ludicrously complex orbit. To top it off, we found 2 such systems already.",
+  },
+  {
+    title: "Nuclear fusion is a chemical reaction in which two atoms fuse into one",
+    description:
+      "This is the process that is responsible for the huge quantities of energy produced in stars. Stars begin as huge balls of the two lightest elements, Hydrogen and helium, and slowly fuse those into heavier and heavier elements.",
+  },
+  {
+    title: "All the chemical elements up to Iron (28th element) are produced by nuclear fusion in the stars",
+    description:
+      "As the combination of particles to form atoms of elements heavier than iron does not result in a net release of energy, fusion reactions stop with iron. Formation of heavier atoms would require energy to supplied instead of a release of energy. This is referred to as the iron limit",
+  },
+  {
+    title: "Supernovae (plural of supernova) are some of the most energetic events in cosmology",
+    description:
+      "They can briefly outshine whole galaxies.",
+  },
+  {
+    title: "The white dwarfs are the remnants of dead stars",
+    description:
+      "These tiny Earth-sized balls are composed of electrons and can shine for millions of years. The heat and energy they emit is only due to their stored thermal energy (i.e. heat), since no nuclear fusion or any other chemical process is present.",
+  },
+  {
+    title: "Sun burns up over four million tons of matter each second in its core",
+    description:
+      "This matter is converted into energy and released in the form of neutrinos and solar radiation.",
+  },
+  {
+    title: "The oldest recorded star is conveniently nicknamed Methuselah",
+    description:
+      "This elderly statesman is estimated to be as old as the universe itself.",
+  },
+  {
+    title: "Neutron stars are the smallest and densest stars that we know of",
+    description:
+      "They are made entirely of neutrons, subatomic particles with no charge, instead of atoms. They are incredibly hot with temperatures of over 600000 Kelvin.",
+  },
+  {
+    title: "Neutron stars can have a radius of about 10 kilometers, but can pack 2 Sun masses in their (comparatively) tiny volume",
+    description:
+      "A teaspoon of neutron matter from the core would weigh around 10 billion tons, which is similar to the weight of an average Earth mountain.",
+  },
+  {
+    title: "Black Holes can be both small and large",
+    description:
+      "Smallest black holes can be the size of an atom, but hold a mass of a large mountain, while the biggest can be larger than Earth.",
+  },
+  {
+    title: "The twinkling of the stars is called scintillation",
+    description:
+      "Stars are so far away, that they are actually just points of light, which means that when they reach the Earth, due to the changes of direction (known as refraction), caused by the atmosphere, their light doesn’t reach any single point, including our eyes and telescopes, continuously.",
+  },
+  {
+    title: "Any non-twinkling lights in our sky are either planets from our system, or artificial satellites orbiting Earth",
+    description:
+      "This is because these objects have actual dimensions, so some light always falls on the same place. Really, we can see planets as little discs of light with a sufficiently powerful telescope.",
+  },
+  {
+    title: " Even planets can scintillate(twinkle)",
+    description:
+      "If we are looking at them when they are low in the sky, near the horizon. This is because the atmosphere is densest in that direction, and there is more of it, so refraction is much higher.",
+  },
+  {
+    title: "Stars do not have uniform composition",
+    description:
+      "Like Earth, and other planets, they have several layers, which have different density, temperature and composition.",
+  },
+  {
+    title: "The surface of a star is called the photosphere",
+    description:
+      "Above it starts the stellar atmosphere- which in turns consists of a Chromosphere and a Corona.",
+  },
+  {
+    title: "Heliosphere is the vast magnetic bubble produced by the Sun",
+    description:
+      "This contains our complete solar system, way beyond the orbit of Pluto.",
+  },
+  {
+    title: "All the stars we see in our night sky are classified into distinct magnitudes",
+    description:
+      "The reason why we perceive stars’ brightness as distinct categories is due to the nature of how brain interprets information we receive with our eyes.",
+  },
+  {
+    title: " Quasars are the most energetic sources, and the most luminous objects in the universe",
+    description:
+      "They can be 100 times brighter than a whole galaxy. Their name is an abbreviation of the term “Quasi-Stellar Radio Sources”.",
+  },
+  {
+    title: "Solar flares can be very dangerous",
+    description:
+      "Solar flares produce streams of charged particles that can damage our satellites, and pose risks to astronauts’ health. This radiation hazard is one of the biggest concerns for all manned missions that might be planned in the future.",
+  },
+  {
+    title: "The great attractor is a mysterious gravitational anomaly",
+    description:
+      "While we are uncertain what it is, we assume that it is probably a large collection of galaxies, or a galactic supercluster, that pulls towards it everything in its neighborhood.",
+  },
+  {
+    title: "Neutrinos are a special sort of elementary particle",
+    description:
+      "These particles are electrically neutral and have virtually no mass. Because they don’t interact with normal matter, they can go through planets and whole stars without slowing down at all. It is incredibly difficult to detect them, and we use huge pools of water infused with cadmium chloride to spot them.",
+  },
+  {
+    title: "The oh-my-god particle is the fastest particle we ever recorded",
+    description:
+      "It’s a proton, one of the two particles that make atomic nucleus, which was moving at 99.99999999999999999999951% speed of light. This single particle with a mass of an electron has the equivalent kinetic energy of a baseball traveling at nearly 100 km/h.",
+  },
+  {
+    title: "Besides the ordinary matter we are all familiar with, there are all kinds of matter that is either theoretical or recently discovered",
+    description:
+      "We now separate matter into baryonic and non baryonic matter, and light and dark matter. Baryonic matter is made of standard atoms and molecules, and subatomic particles like protons, neutrons and electrons.",
+  },
   // {
   //   title: "",
   //   description:
