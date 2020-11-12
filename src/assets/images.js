a1d7435878a4ae96ad20422e3b40dcee3a45f49f
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-11-12 13:09:45 +0530

    Improved embed of explore, added images

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,12 +1,74 @@
 module.exports = {
-  moon:
-    "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/moon.png?alt=media&token=1a9b00d7-d386-4983-912c-3cd812c9d06b",
-  sun:
-    "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/sun.png?alt=media&token=5fbe4ee9-fe34-4afd-98b1-9ea573f2368b",
-  earth:
-    "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/earth.png?alt=media&token=86cc06ce-06b7-4dd4-b0b9-172ceaf2bd7e",
-  mars:
-    "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/mars.png?alt=media&token=8f7d6932-dcb6-4de2-8f1f-cb45b22da8e8",
-  jupiter:
-    "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/jupiter.png?alt=media&token=e09e6220-50cb-4b7f-945a-5f6efdf754cf",
+  moon: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/moon.png?alt=media&token=1a9b00d7-d386-4983-912c-3cd812c9d06b",
+    description:
+      "The brightest and largest object in our night sky, the Moon makes Earth a more livable planet by moderating our home planet's wobble on its axis, leading to a relatively stable climate. It also causes tides, creating a rhythm that has guided humans for thousands of years. The Moon was likely formed after a Mars-sized body collided with Earth.",
+  },
+  sun: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/sun.png?alt=media&token=5fbe4ee9-fe34-4afd-98b1-9ea573f2368b",
+    description:
+      "The Sun is the star at the center of the Solar System. It is a nearly perfect sphere of hot plasma, heated to incandescence by nuclear fusion reactions in its core, radiating the energy mainly as light and infrared radiation.",
+  },
+  mercury: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/mercury.png?alt=media&token=9f8c8d77-2181-4336-8e43-b1eedb9ba8bd",
+    description:
+      "The smallest planet in our solar system and nearest to the Sun, Mercury is only slightly larger than Earth's Moon. From the surface of Mercury, the Sun would appear more than three times as large as it does when viewed from Earth, and the sunlight would be as much as seven times brighter.",
+  },
+  venus: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/venus.png?alt=media&token=c2255fe4-254b-4a6b-9b9e-f8e5cdab5205",
+    description:
+      "Similar in size and structure to Earth, Venus has been called Earth's twin. These are not identical twins, however – there are radical differences between the two worlds. Venus has a thick, toxic atmosphere filled with carbon dioxide and it’s perpetually shrouded in thick, yellowish clouds of mostly sulfuric acid that trap heat, causing a runaway greenhouse effect. It’s the hottest planet in our solar system.",
+  },
+  earth: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/earth.png?alt=media&token=86cc06ce-06b7-4dd4-b0b9-172ceaf2bd7e",
+    description:
+      "Our home planet is the third planet from the Sun, and the only place we know of so far that’s inhabited by living things. While Earth is only the fifth largest planet in the solar system, it is the only world in our solar system with liquid water on the surface. Just slightly larger than nearby Venus, Earth is the biggest of the four planets closest to the Sun, all of which are made of rock and metal.",
+  },
+  mars: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/mars.png?alt=media&token=8f7d6932-dcb6-4de2-8f1f-cb45b22da8e8",
+    description:
+      "The fourth planet from the Sun, Mars is a dusty, cold, desert world with a very thin atmosphere. This dynamic planet has seasons, polar ice caps, canyons, extinct volcanoes, and evidence that it was even more active in the past.",
+  },
+  jupiter: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/jupiter.png?alt=media&token=e09e6220-50cb-4b7f-945a-5f6efdf754cf",
+    description:
+      "Jupiter has a long history surprising scientists—all the way back to 1610 when Galileo Galilei found the first moons beyond Earth. That discovery changed the way we see the universe. Fifth in line from the Sun, Jupiter is, by far, the largest planet in the solar system – more than twice as massive as all the other planets combined.",
+  },
+  saturn: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/saturn.png?alt=media&token=040272ba-7c8e-4abf-8dc4-d00ec7f64390",
+    description:
+      "Saturn is the sixth planet from the Sun and the second largest planet in our solar system. Adorned with thousands of beautiful ringlets, Saturn is unique among the planets. It is not the only planet to have rings—made of chunks of ice and rock—but none are as spectacular or as complicated as Saturn's.",
+  },
+  uranus: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/uranus.png?alt=media&token=13689a30-f2b9-4cf4-a473-2fb68f36d062",
+    description:
+      "Uranus is the seventh planet from the sun and the first to be discovered by scientists. Although Uranus is visible to the naked eye, it was long mistaken as a star because of the planet's dimness and slow orbit. The planet is also notable for its dramatic tilt, which causes its axis to point nearly directly at the sun.",
+  },
+  neptune: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/neptune.png?alt=media&token=03e1736d-93d7-4b4f-8f19-21f10ded711a",
+    description:
+      "Dark, cold and whipped by supersonic winds, ice giant Neptune is the eighth and most distant planet in our solar system. More than 30 times as far from the Sun as Earth, Neptune is the only planet in our solar system not visible to the naked eye and the first predicted by mathematics before its discovery. In 2011 Neptune completed its first 165-year orbit since its discovery in 1846.",
+  },
+  phobos: {
+    link:
+      "https://firebasestorage.googleapis.com/v0/b/astronomia-c2f5d.appspot.com/o/phobos.jpg?alt=media&token=60f48d9a-ef0b-49ac-aca9-1336d1856ac8",
+    description:
+      "Phobos is the innermost and larger of the two natural satellites of Mars, the other being Deimos. Both moons were discovered in 1877 by American astronomer Asaph Hall. Phobos is a small, irregularly shaped object with a mean radius of 11 km and is seven times more massive than the outer moon, Deimos.",
+  },
+  deimos: {
+    link:
+      "https://upload.wikimedia.org/wikipedia/commons/8/8d/Deimos-MRO.jpg",
+    description:
+      "Deimos is the smaller and outermost of the two natural satellites of Mars, the other being Phobos. Deimos has a mean radius of 6.2 km (3.9 mi) and takes 30.3 hours to orbit Mars. Deimos is 23,460 km (14,580 mi) from Mars, much further than Mars's other moon, Phobos. It is named after Deimos, the Ancient Greek god and personification of dread and terror, and who is also a son of Mars and twin brother of Phobos.",
+  },
 };
