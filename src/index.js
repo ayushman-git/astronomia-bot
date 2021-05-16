096e609c752d6b6ef356610a08ceae68a16331de
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-05-16 12:25:01 +0530

    created firebse file to handle firebase init

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 const { Client } = require("discord.js");
 require("dotenv").config();
-const admin = require("firebase-admin");
-admin.initializeApp({
-  credential: admin.credential.cert({
-    type: "service_account",
-    project_id: "astronomia-c2f5d",
-    private_key_id: process.env.FIRESTORE_PRIVATE_KEY_ID,
-    private_key: process.env.FIRESTORE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    client_email: process.env.FIRESTORE_CLIENT_EMAIL,
-    client_id: "104413135440882877790",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: process.env.FIREBASE_CERT,
-  }),
-});
 
-const db = new admin.firestore();
+const { db } = require("./db/firebase");
 const messageHandler = require("./events/message");
 const clientReadySetup = require("./events/clientReady");
 const guildCreateMessage = require("./events/guildCreate");
